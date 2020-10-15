b25e5b5494de0171dc03a4c0be0bfb3dcef1c930
BigFrager <don.frye@outlook.com>
2020-10-14 22:22:36 -0400

    hits them all

--- Utilities/ssh_copy_id.ps1
+++ Utilities/ssh_copy_id.ps1
@@ -1,1 +1,11 @@
-Get-Content $env:USERPROFILE\.ssh\id_rsa.pub | ssh pi@pi-hole "cat >> .ssh/authorized_keys"
+$myPi = @("pi-hole","woody","greenlantern","pizerored","legion","octopi","rabble")
+#$myPi = @("woody")
+$myKey = Get-Content $env:USERPROFILE\.ssh\id_rsa.pub
+Write-Output($myPi.Count)
+Write-Output($myPi)
+Write-Output($myKey)
+ForEach($myHost in $myPi)
+{ 
+#ssh pi@$myHost "grep '$myKey' .ssh/authorized_keys"
+ssh pi@$myPi "cat $myKey >> .ssh/authorized_keys"
+}
